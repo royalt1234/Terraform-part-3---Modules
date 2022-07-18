3e61e1f0aa2cbcde4c18410d1433fdaed2120fc4
royalt1234 <thursdayfrip@gmail.com>
2022-07-18 09:52:48 +0100

    first commit

---
+++ modules/RDS/variables.tf
@@ -0,0 +1,34 @@
+
+variable "db-username" {
+  type        = string
+  description = "The master user name"
+}
+
+
+variable "db-password" {
+  type        = string
+  description = "Master password"
+}
+
+variable "db-sg" {
+  type = list
+  description = "The DB security group"
+}
+
+variable "private_subnets" {
+  type        = list
+  description = "Private subnets fro DB subnets group"
+}
+
+
+variable "tags" {
+  description = "A mapping of tags to assign to all resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "db_name" {
+  type        = string
+  description = "name of rds db"
+  default     = "mosesdb"
+}
